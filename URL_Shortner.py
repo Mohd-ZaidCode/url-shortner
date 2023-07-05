2728dda6e5cd833a247cd929f871ec3761a8c605
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2023-07-05 14:26:41 +0530

    Update URL_Shortner.py
    Formating and commenting of code for the better understanding

--- URL_Shortner.py
+++ URL_Shortner.py
@@ -1,10 +1,14 @@
+#Import pyshortner library to impliment for the url shorting
 import pyshorteners
 
+#Taking user input 
 Userer_enterd_url = input('Please Enter The URL: ')
 
+#Function take input and print shorted url
 def url_Shortner(url):
+    #Creating a variable of pyshortner library to use it for url shortening
     pshort = pyshorteners.Shortener()
     print(pshort.tinyurl.short(url))
 
-
+#Calling url shortening function
 url_Shortner(Userer_enterd_url)
